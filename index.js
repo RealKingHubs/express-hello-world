238743a26f9fafa845c508a3c8b0f9d86354df60
RealKingHubs <odokingsleyu@gmail.com>
2026-06-04 18:17:05 +0100

    fix: update Listening Port to 80

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
-  console.log('Visit http://localhost:3000 to see your beautiful homepage!')
+app.listen(80, function () {
+  console.log('Listening on port 80...')
+  console.log('Visit your public IP to see your beautiful homepage!')
 })
